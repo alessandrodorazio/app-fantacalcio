5ddbca18ad2f0d7865e35969f907048da3b7768e
Alessandro D'Orazio <me@alessandrodorazio.it>
2024-09-07 20:37:48 +0200

    fix

--- server/api/database.js
+++ server/api/database.js
@@ -2,7 +2,8 @@ import { createRouter, defineEventHandler } from "h3";
 import Database from "better-sqlite3";
 
 export default defineEventHandler(async (event) => {
-  const db = new Database("../database/players_database.sqlite");
+  const dbPath = path.join(process.cwd(), "database/players_database.sqlite");
+  const db = new Database(dbPath, { readonly: true });
   const query = "SELECT * FROM players";
   const result = db.prepare(query).all();
   db.close();
