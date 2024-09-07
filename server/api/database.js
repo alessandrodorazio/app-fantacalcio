fb323a7f93065af73207407678fce4f93b0b12e5
Alessandro D'Orazio <me@alessandrodorazio.it>
2024-09-07 21:31:34 +0200

    Revert "fix"
    This reverts commit 5ddbca18ad2f0d7865e35969f907048da3b7768e.

--- server/api/database.js
+++ server/api/database.js
@@ -2,8 +2,7 @@ import { createRouter, defineEventHandler } from "h3";
 import Database from "better-sqlite3";
 
 export default defineEventHandler(async (event) => {
-  const dbPath = path.join(process.cwd(), "database/players_database.sqlite");
-  const db = new Database(dbPath, { readonly: true });
+  const db = new Database("../database/players_database.sqlite");
   const query = "SELECT * FROM players";
   const result = db.prepare(query).all();
   db.close();
